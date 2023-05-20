85e02872de4832fca36ec8bc1f8e7b37453761ec
Tri Cahya Fitriansyah <tricahya644@gmail.com>
2023-05-20 20:00:32 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Arkatama Laravel</title>
+</head>
+<body>
+    <div class="container">
+        <table class="table mt-5">
+            <thead>
+            <tr class="table-secondary">
+                <th scope="col">#</th>
+                <th scope="col">Aksi</th>
+                <th scope="col">Avatar</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Role</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="" alt="" width="100px"></td>
+                <td>Tri Cahya</td>
+                <td>tricahya@gmail.com</td>
+                <td>085793</td>
+                <td>Admin</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="" alt="" width="100px"></td>
+                <td>Riki</td>
+                <td>riki@gmail.com</td>
+                <td>081298</td>
+                <td>Staff</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="" alt="" width="100px"></td>
+                <td>Ridwan</td>
+                <td>ridwan@gmail.com</td>
+                <td>082323</td>
+                <td>Staff</td>
+            </tr>
+            <tr>
+            <th scope="row">4</th>
+                <td>
+                    <a href="" class="btn btn-primary">Detail</a>
+                    <a href="" class="btn btn-warning">Edit</a>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
+                <td><img src="" alt="" width="100px"></td>
+                <td>Rian</td>
+                <td>rian4@gmail.com</td>
+                <td>083824</td>
+                <td>Admin</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
